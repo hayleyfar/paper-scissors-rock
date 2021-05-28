1f0a9e844d4ad0c72b750c0bb56ca8eb6fb89a65
hayleyfar <66756697+hayleyfar@users.noreply.github.com>
2021-05-28 17:55:28 +1200

    Update paper scissors rock.py

--- paper scissors rock.py
+++ paper scissors rock.py
@@ -1,1 +1,4 @@
-print("paper, scissors, rock game")
+#paper scissors rock game
+print("Welcome to my paper scissors rock game!")
+program_answers = ["paper","scissors","rock"]
+users_choice = input("paper, scissors or rock?")
