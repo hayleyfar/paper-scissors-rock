9e136bde8fcbe69bc5cb6d404576c1191184d5bb
hayleyfar <66756697+hayleyfar@users.noreply.github.com>
2021-06-01 17:28:48 +1200

    Update paper scissors rock.py
    first draft. only runs once

--- paper scissors rock.py
+++ paper scissors rock.py
@@ -1,4 +1,37 @@
 #paper scissors rock game
-print("Welcome to my paper scissors rock game!")
+import random
+print("Welcome to my paper scissors rock game! \nYou will be asked paper, scissors or rock and the program will also pick as well :D")
+#list
 program_answers = ["paper","scissors","rock"]
-users_choice = input("paper, scissors or rock?")
+print("\n")
+users_choice = input("paper, scissors or rock? \nYour answer: ")
+#random word from list
+program = random.choice(program_answers)
+print("Program answer: " ,program )
+#tie for all paper, scissors or rock answers
+if users_choice == program:
+ print("tie")
+
+while users_choice == "paper":
+ if program == "scissors":
+   print("you lose")
+   break #stop code
+ elif program == "rock":
+   print("you win!")
+   break
+
+while users_choice == "scissors":
+ if program == "paper":
+   print("you win!")
+   break
+ elif program == "rock":
+   print("you lose")
+   break
+
+while users_choice == "rock":
+ if program == "scissors":
+   print("you win!")
+   break
+ elif program == "paper":
+   print("you lose")
+   break
